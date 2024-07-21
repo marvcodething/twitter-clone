d7a3a8b8897346fceb3bf3a5073a6f044808e0d3
marvcodething <mromero3@oxy.edu>
2024-07-20 23:54:51 -0400

    4 - deployment

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,48 @@
+import path from "path";
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
-import connectMongoDB from "./db/connectMongoDB.js";
-import bcrypt from 'bcrypt';
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
+
+import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import notificationRoutes from "./routes/notification.route.js";
-import {v2 as cloudinary} from "cloudinary";
+
+import connectMongoDB from "./db/connectMongoDB.js";
+
 dotenv.config();
+
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(express.json({limit:"5mb"})); //middleware to parse req.body
-app.use(express.urlencoded({extended: true})); //middleware to parse urlencoded data
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+// limit shouldn't be too high to prevent DOS
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
-app.use(cookieParser()); //middleware to parse cookies
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-app.listen(PORT,() => {
-    console.log(`server is up and running on port ${PORT}`);
-    connectMongoDB();
-})
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+	connectMongoDB();
+});
